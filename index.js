db6d74a18791611c159bbfcf5dba805ca6cb2e13
Bruno Nogueira Nascimento Wowk <bruno.wowk@upx.com>
2018-09-14 11:27:15 -0300

    One Ada to rule them all

--- index.js
+++ index.js
@@ -2,6 +2,21 @@
 const console = require('console');
 const process = require('child_process');
 
-process.execSync('cp '+__dirname+`/ada.jpg /tmp/ada.jpg && sqlite3 ~/Library/Application\\ Support/Dock/desktoppicture.db "DELETE FROM preferences WHERE key IN (9, 11); UPDATE data SET value = '/tmp/ada.jpg'" && killall Dock`);
+process.execSync(`
+cp ${__dirname}/ada.jpg /tmp/ada.jpg &&
+sqlite3 ~/Library/Application\\ Support/Dock/desktoppicture.db "
+    DELETE FROM data;
+    DELETE FROM displays;
+    DELETE FROM pictures;
+    DELETE FROM preferences;
+    DELETE FROM prefs;
+    DELETE FROM spaces;
+    INSERT INTO pictures (space_id, display_id) VALUES (null, null);
+    INSERT INTO data (value) VALUES ('/tmp/ada.jpg');
+    INSERT INTO preferences (key, data_id, picture_id) VALUES (1, 1, 1);
+    INSERT INTO data (value) VALUES (1);
+    INSERT INTO preferences (key, data_id, picture_id) VALUES (2, 2, 1);
+" && killall Dock
+`)
 console.log("Meowww!");
 
