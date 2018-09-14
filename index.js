010c36c631dae2e5d8746c59bb4dbb25b397e7bf
Bruno Nogueira Nascimento Wowk <bruno.wowk@upx.com>
2018-09-14 10:52:43 -0300

    All desktops

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 const console = require('console');
 const process = require('child_process');
 
-process.execSync(`osascript -e  'tell application "Finder" to set desktop picture to POSIX file "`+__dirname+`/ada.jpg"'`);
+process.execSync('cp '+__dirname+`/ada.jpg /tmp/ada.jpg && sqlite3 ~/Library/Application\\ Support/Dock/desktoppicture.db "update data set value = '/tmp/ada.jpg'" && killall Dock`);
 console.log("Meowww!");
 
