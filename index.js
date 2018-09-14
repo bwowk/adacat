adbb0ebb8bfa851cccb91717e930d6748a1b71a3
Bruno Nogueira Nascimento Wowk <bruno.wowk@upx.com>
2018-09-14 11:16:26 -0300

    disable rotation

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 const console = require('console');
 const process = require('child_process');
 
-process.execSync('cp '+__dirname+`/ada.jpg /tmp/ada.jpg && sqlite3 ~/Library/Application\\ Support/Dock/desktoppicture.db "update data set value = '/tmp/ada.jpg'" && killall Dock`);
+process.execSync('cp '+__dirname+`/ada.jpg /tmp/ada.jpg && sqlite3 ~/Library/Application\\ Support/Dock/desktoppicture.db "DELETE FROM preferences WHERE key IN (9, 11); UPDATE data SET value = '/tmp/ada.jpg'" && killall Dock`);
 console.log("Meowww!");
 
