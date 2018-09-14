8ea6a8cd8706365fcdb3a28b4f3c666bc05d659f
Bruno Nogueira Nascimento Wowk <bruno.wowk@upx.com>
2018-09-14 10:21:31 -0300

    Meow

---
+++ index.js
@@ -0,0 +1,6 @@
+const console = require('console');
+const process = require('child_process');
+
+process.execSync(`osascript -e  'tell application "Finder" to set desktop picture to POSIX file "`+__dirname+`/ada.jpg"'`);
+console.log("Meowww!");
+
